e1fd5a81ebdd07cc74ce137ff98e48f9e524c959
MrEMath <michael.j.englade@gmail.com>
2026-02-15 14:55:45 -0600

    anon key

--- supabase-client-triangles.js
+++ supabase-client-triangles.js
@@ -1,6 +1,6 @@
 // supabase-client-triangles.js
 const SUPABASE_URL = "https://xsmhhduixpyotdhsjizr.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_PUBLIC_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhzbWhoZHVpeHB5b3RkaHNqaXpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA2NDU0NTYsImV4cCI6MjA4NjIyMTQ1Nn0.BKVW6GfqJfDfp1UKPMNjehZ4UcF_D7ivLYmrXY12_60";
 
 window.supabaseTriangleClient = supabase.createClient(
   SUPABASE_URL,
